dc1c8a4e6c594fc9b97a57e6f7cc5f494adf773f
codekyn <codekyn@gmail.com>
2017-08-11 12:59:58 +0200

    End tutorial with out errors Djangogirls

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import redirect
 # Create your views here
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -26,7 +26,7 @@ def post_new(request):
         	post.author = request.user
         	post.published_date = timezone.now()
         	post.save()
-		return redirect('blog.views.post_detail', pk=post.pk)
+		return redirect('post_detail', pk=post.pk)
 	else:
 	    form = PostForm()
 		
@@ -40,7 +40,7 @@ def post_edit(request, pk):
 			post = form.save(commit=False)
 			post.author = request.user
 			post.save()
-			return redirect('blog.views.post_detail', pk = post.pk)
+			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 		
